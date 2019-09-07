e4230553c6251915c1761583dd2bb674fcebb870
moutine <44560663+moutine@users.noreply.github.com>
2019-09-07 19:52:32 +0800

    解决跨域问题
    status=0

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  devServer:{
-    disableHostCheck: true
+  devServer: {
+    disableHostCheck: true,
+    proxy: {
+      '/api': {
+        target: 'http://47.100.16.60:8080',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
   }
 }
